bda16ee8e3c0f54a431d3626d16a3dcce6142c1e
goldi713 <kumarigoldi995@gmail.com>
2026-08-09 09:13:07 +0530

    Add basic alarm and reminder functions in script.js

---
+++ script.js
@@ -0,0 +1,13 @@
+// CareAlarm - Basic JavaScript
+
+function setAlarm() {
+    alert("⏰ Alarm feature is ready!");
+}
+
+function setMedicineReminder() {
+    alert("💊 Medicine reminder is ready!");
+}
+
+function emergencyAlert() {
+    alert("🚨 Emergency alert activated!");
+}
